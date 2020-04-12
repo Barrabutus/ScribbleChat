1fd8e43356dd3c2ac09106bc12467ae94207171e
Barrabutus <62641841+Barrabutus@users.noreply.github.com>
2020-04-12 11:31:39 +0100

    LobbyScene and room updates.
    Lobby scene should resize to fit screen size,s better.
    
    Chat should be working
    
    User should be redirected to room when they create a room.
    
    Added new scene GameScene

---
+++ Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using Photon.Chat;
+
+
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+    
+    public string namePrefix;
+    
+    [Header("Script References")]
+    public ChatManager chatManager;
+    public RoomManager roomManager;
+    public LobbyManager lobbyManager; 
+    public ConnectionManager instance;
+    public Text roomConnectionStateLabel;
+    private string username;
+
+ 
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        chatManager = GameObject.Find("ChatManager").GetComponent<ChatManager>();
+        //roomManager = GameObject.Find("RoomManager").GetComponent<RoomManager>();
+        lobbyManager = GameObject.Find("LobbyManager").GetComponent<LobbyManager>();
+        PhotonNetwork.ConnectUsingSettings();
+    }
+ 
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Joined Master....Going to lobby");
+        PhotonNetwork.JoinLobby();
+        roomConnectionStateLabel.text = "Connected to room!";
+    }
+ 
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("We have joined the lobby getting list of rooms....");
+        //Append a random number to a name so we can identify the user in the room scene.
+        username = namePrefix+Random.Range(0000,99999);
+        PhotonNetwork.NickName = username;
+        Debug.Log("Welcome " + username);
+        chatManager.ConnectToChatServer();
+        chatManager.SetUsername(username);
+        lobbyManager.SetUsernameLabels();
+        PhotonNetwork.AutomaticallySyncScene = true;
+        instance = this;
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        
+      //  roomConnectionStateLabel.text = "Disconnected from room server...";
+
+    }
+
+    
+
+}
---
+++ Assets/Scripts/GameRoomManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using Photon.Chat;
+using ExitGames.Client.Photon;
+
+
+public class GameRoomManager : MonoBehaviourPunCallbacks
+{
+        public Transform userPanel;
+        public GameObject userPrefab;
+        private int numPlayers;
+        public List<int> currentRoomPlayers =new List<int>();
+        private static GameRoomManager instance;
+
+        private ConnectionManager connectionManager;
+
+        public bool isRoomFull;
+        public GameObject rdyBtn;
+        //public _LobbyManager lobbyManager;
+        //public _ChatManager chatManager;
+
+        private void Start() {
+            instance = this;
+            connectionManager = GameObject.Find("ConnectionManager").GetComponent<ConnectionManager>();
+            
+
+        }
+        
+     
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("User " + PhotonNetwork.NickName + " has joined the room...");
+            StartCoroutine(RunUserListUpdate());
+            //numPlayers  = PhotonNetwork.CurrentRoom.PlayerCount; 
+           // if(numPlayers == 1) {
+            //    chatManager.SetChatChannelName(PhotonNetwork.CurrentRoom.Name);
+                
+          //  }
+           // chatManager.SetChatClientName(PhotonNetwork.NickName);
+           // chatManager.client.PublishMessage(PhotonNetwork.CurrentRoom.Name, PhotonNetwork.NickName + " joined the room");
+            
+
+        
+        }
+
+        void Update() 
+        {
+           
+        }
+
+        
+        IEnumerator RunUserListUpdate()
+        {
+            while(true)
+            {
+                if(PhotonNetwork.CurrentRoom.PlayerCount != numPlayers)
+                {
+                    Dictionary<int, Photon.Realtime.Player> pList = Photon.Pun.PhotonNetwork.CurrentRoom.Players;
+                    foreach (KeyValuePair<int, Photon.Realtime.Player> p in pList)
+                    {
+                        if(!currentRoomPlayers.Contains(p.Value.ActorNumber))
+                        {
+                            GameObject listing = Instantiate(userPrefab, userPanel.transform);
+                            listing.name = p.Value.ActorNumber.ToString();
+                            Text btnText = listing.GetComponent<Text>();     
+                            btnText.text = p.Value.NickName;
+                            listing.transform.SetParent(userPanel.transform);
+                             //Add to the list of current players in the room.
+                            currentRoomPlayers.Add(p.Value.ActorNumber);
+                            numPlayers = PhotonNetwork.CurrentRoom.PlayerCount; 
+                        }
+                    }                                 
+                }
+                yield return new WaitForSeconds(2f);
+            }
+        }
+
+        
+    public override void OnDisconnected(DisconnectCause cause){
+        Debug.Log(PhotonNetwork.NickName + " has disconnected. " + cause.ToString());
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log(otherPlayer.NickName + " has left, disconnected or closed the game");
+        //Stop the routine because we are modifying the list. 
+        StopCoroutine(RunUserListUpdate());
+        foreach(int x in currentRoomPlayers)
+        {
+            if(x == otherPlayer.ActorNumber)
+            {
+                //currentRoomPlayers.Remove(x);
+                GameObject playerListItem = GameObject.Find(otherPlayer.ActorNumber.ToString());
+                Destroy(playerListItem);
+            }
+        }
+        //restart the routine
+        StartCoroutine(RunUserListUpdate());
+    }
+
+    public void OnClickLeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+
+        PhotonNetwork.LoadLevel("LobbyScene");
+
+    }
+
+}
